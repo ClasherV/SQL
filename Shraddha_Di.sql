e6e23d7bff82d7adfb1bc3c2aa631778b8f97207
ClasherV <vaibhavraikwar505@gmail.com>
2024-07-08 16:37:56 +0530

    Added Querries

---
+++ Shraddha_Di.sql
@@ -0,0 +1,15 @@
+/*
+-- CREATE DATABASE db_name;
+-- USE db_name;
+-- CREATE TABLE Table_Name(Column_Name1 int PRIMARY KEY,Column_Name2 varchar(50),Column_Name3 int NOT NULL);
+-- INSERT into Table_Name VALUES(1,'Name1',15);
+-- INSERT into Table_Name VALUES(2,'Name2',16);
+-- INSERT into Table_Name VALUES(3,'Name3',17);
+-- SELECT * FROM Table_Name;
+-- DROP DATABASE db_name;
+*/
+
+/*
+-- TinyInt Signed
+-- TinyInt UNSIGNED
+*/
